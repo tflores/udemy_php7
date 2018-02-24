afd3370a8f80f93c928ecb28c5d234e9dff55750
Tiago Flores <tiago.flores@yahoo.com.br>
2018-02-23 21:03:39 -0300

    Segundo commit teste

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # udemy_php7
 Curso PHP7 Udemy
+
+## Códigos produzidos para estudos de PHP7
+
--- teste.php
+++ teste.php
@@ -1,6 +1,15 @@
+<?php session_start();?>
+<html>
+<head>
+	<title>Página de Teste</title>
+</head>
+
+<body>
+	<h1>Página de teste</h1>
+	<p>Teste <?php echo session_id(); ?></p>	
+</body>
+
+</html>
 <?php
-
-echo phpinfo();
-
 
 ?>
